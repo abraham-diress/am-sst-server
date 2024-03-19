c688d214291b0ef902b148a1cfd57cfae97c1938
Abraham Diress <a.diress@alustudent.com>
2024-03-19 12:17:09 +0200

    modify cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://am-sst.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
